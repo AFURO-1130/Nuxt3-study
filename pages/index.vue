6a40b68b5a667fd7a87b8c0cb37c504a76102b55
AFURO-1130 <abcshotaro616@gmail.com>
2023-01-04 11:32:03 +0900

    introduce nuxt-icon module

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
+<script setup>
+</script>
 <template>
   <div>
     <Header>Nuxt3入門</Header>
+    <!-- これだけでアイコンの読み込みできるの神 -->
+    <Icon name="uil:github"></Icon>
+    <Icon name="NuxtIcon"></Icon>
+    <Icon name="😄"></Icon>
     <br />
     <List />
     <Footer></Footer>
   </div>
 </template>
-<script setup="ts"></script>
+
